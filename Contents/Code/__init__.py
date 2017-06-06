f2c4d0b82fc0edf4a330b223a5d03724a87d8d21
daeks <daeks@gmx.de>
2017-06-06 12:09:59 +0200

    provide tv shows support

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -8,7 +8,36 @@ def Start():
   
 def ValidatePrefs():
   pass
+  
+def loadStaticMedia(metadata, media):
+  part = media.items[0].parts[0]
+  (root_file, ext) = os.path.splitext(os.path.basename(part.file))
+  metadata.title = root_file
 
+  if Prefs['static_poster']:  
+    if Prefs['static_poster_path']:
+      data = Core.storage.load(Prefs['static_poster_path'])
+    else:
+      data = Core.storage.load(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), STATIC_POSTER))
+    
+    media_hash = hashlib.md5(data).hexdigest()
+
+    if media_hash not in metadata.posters:
+      metadata.posters[media_hash] = Proxy.Media(data, sort_order=1)
+      Log('Static poster added for %s' % metadata.title)
+  
+  if Prefs['static_background']:  
+    if Prefs['static_background_path']:
+      data = Core.storage.load(Prefs['static_background_path'])
+    else:
+      data = Core.storage.load(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), STATIC_BACKGROUND))
+    
+    media_hash = hashlib.md5(data).hexdigest()
+
+    if media_hash not in metadata.art:
+      metadata.art[media_hash] = Proxy.Media(data, sort_order=1)
+      Log('Static background added for %s' % metadata.title)
+      
 class StaticMediaAgent(Agent.Movies):
 
   name = 'Static Media'
@@ -23,30 +52,20 @@ class StaticMediaAgent(Agent.Movies):
 
   def update(self, metadata, media, lang):
   
-    part = media.items[0].parts[0]
-    (root_file, ext) = os.path.splitext(os.path.basename(part.file))
-    metadata.title = root_file
+    loadStaticMedia(metadata, media)
+  
+class StaticMediaAgent(Agent.TV_Shows):
 
-    if Prefs['static_poster']:  
-      if Prefs['static_poster_path']:
-        data = Core.storage.load(Prefs['static_poster_path'])
-      else:
-        data = Core.storage.load(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), STATIC_POSTER))
-      
-      media_hash = hashlib.md5(data).hexdigest()
+  name = 'Static Media'
+  languages = [Locale.Language.NoLanguage]
+  primary_provider = True
+  accepts_from = ['com.plexapp.agents.localmedia']
 
-      if media_hash not in metadata.posters:
-        metadata.posters[media_hash] = Proxy.Media(data, sort_order=1)
-        Log('Static poster added for %s' % metadata.title)
-    
-    if Prefs['static_background']:  
-      if Prefs['static_background_path']:
-        data = Core.storage.load(Prefs['static_background_path'])
-      else:
-        data = Core.storage.load(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), STATIC_BACKGROUND))
-      
-      media_hash = hashlib.md5(data).hexdigest()
+  def search(self, results, media, lang):
 
-      if media_hash not in metadata.art:
-        metadata.art[media_hash] = Proxy.Media(data, sort_order=1)
-        Log('Static background added for %s' % metadata.title)  
+    Log('Searching for %s' % media.name)
+    results.Append(MetadataSearchResult(id = media.id, name = media.name, year = None, score = 100, lang = lang))
+
+  def update(self, metadata, media, lang):
+  
+    loadStaticMedia(metadata, media)
